72553f2bbd89d3afe7dd9714414e998483fad8e2
parvez <parvezyesrat17032024@gmail.com>
2025-08-13 22:08:47 +0600

    added 2nd

---
+++ Problem-02.js
@@ -0,0 +1,11 @@
+function onlyCharacter(str){
+  if (typeof str !=="string"){
+    return "Invalid";
+  }
+  let str2="";
+  for(const s of str ){
+    if(s!==" ") str2+=s;
+  }
+  return str2.toUpperCase();
+}
+console.log(onlyCharacter("Cy   bar- At  tac k  "));
